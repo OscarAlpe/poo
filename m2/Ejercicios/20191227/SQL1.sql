62a309eb5cf90bf6ef1f83f22afa8a3a8a81e9b4
Oscar Megía López <pfgytk@gmail.com>
2019-12-27 14:05:19 +0100

    Ejercicios en SQL con dbFORGE Studio 2019 para PostgreSQL en academia

---
+++ m2/Ejercicios/20191227/SQL1.sql
@@ -0,0 +1,15 @@
+﻿/*
+CREATE TABLE weather (
+    city            varchar(80),
+    temp_lo         int,           -- low temperature
+    temp_hi         int,           -- high temperature
+    prcp            real,          -- precipitation
+    date            TIMESTAMP
+);
+*/
+--SELECT * FROM weather;
+--INSERT INTO weather VALUES ('Torrelavega', 3, 20, 9.0, datetime_pl('2019-12-27', '09:00:00'));
+--DROP TABLE weather;
+
+--CREATE DATABASE LIBRO;
+
