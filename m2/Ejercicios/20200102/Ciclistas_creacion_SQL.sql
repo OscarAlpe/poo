7191369b43051a89e4e20b3bdbc29ee474f8fc05
Oscar Megía López <pfgytk@gmail.com>
2020-01-02 11:10:48 +0100

    Ejercicios en SQL con dbFORGE Studio 2019 para PostgreSQL (Hoja 1 - Consultas de selección 1) en academia

---
+++ m2/Ejercicios/20200102/Ciclistas_creacion_SQL.sql
@@ -0,0 +1,66 @@
+﻿CREATE DATABASE ciclistas;
+
+-- -------------------------------------
+-- Tables
+
+CREATE TABLE ciclista (
+  dorsal SMALLINT NOT NULL,
+  nombre VARCHAR(30) NOT NULL,
+  edad SMALLINT NULL,
+  nomequipo VARCHAR(25) NOT NULL,
+  PRIMARY KEY (dorsal) );
+
+CREATE INDEX equipociclista ON ciclista(nomequipo);
+
+CREATE TABLE equipo (
+  nomequipo VARCHAR(25) NOT NULL,
+  director VARCHAR(30) NULL,
+  PRIMARY KEY (nomequipo)
+);
+
+CREATE TABLE etapa (
+  numetapa SMALLINT NOT NULL,
+  kms SMALLINT NOT NULL,
+  salida VARCHAR(35) NOT NULL,
+  llegada VARCHAR(35) NOT NULL,
+  dorsal SMALLINT NULL,
+  PRIMARY KEY (numetapa)
+);
+
+CREATE INDEX ciclistaetapa ON etapa(dorsal);
+
+CREATE TABLE lleva (
+  dorsal SMALLINT NOT NULL,
+  numetapa SMALLINT NOT NULL,
+  codigo VARCHAR(3) NOT NULL,
+  PRIMARY KEY (numetapa, codigo)
+);
+
+CREATE INDEX ciclistallevar ON lleva(dorsal);
+CREATE INDEX etapallevar ON lleva(numetapa);
+CREATE INDEX maillotllevar ON lleva(codigo);
+
+CREATE TABLE maillot (
+  codigo VARCHAR(3) NOT NULL,
+  tipo VARCHAR(30) NOT NULL,
+  color VARCHAR(20) NOT NULL,
+  premio INT NOT NULL,
+  PRIMARY KEY (codigo)
+);
+
+CREATE TABLE puerto (
+  nompuerto VARCHAR(35) NOT NULL,
+  altura SMALLINT NOT NULL,
+  categoria VARCHAR(1) NOT NULL,
+  pendiente DOUBLE PRECISION NULL,
+  numetapa SMALLINT NOT NULL,
+  dorsal SMALLINT NULL,
+  PRIMARY KEY (nompuerto)
+);
+
+CREATE INDEX ciclistapuerto ON puerto(dorsal);
+CREATE INDEX etapapuerto ON puerto(numetapa);
+
+-- ----------------------------------------------------------------------
+-- EOF
+
---
+++ m2/Ejercicios/20200102/Hoja 1 - Consultas y seleccion 1_SQL.sql
@@ -0,0 +1,14 @@
+﻿-- 1.1 Listar las edades de los ciclistas (sin repetidos)
+SELECT DISTINCT edad FROM ciclista;
+
+-- 1.2 Listar las edades de los ciclistas de Artiach
+SELECT edad FROM ciclista WHERE nomequipo = 'Artiach';
+
+-- 1.3 Listar las edades de los ciclistas de Artiach o de Amore Vita
+SELECT edad FROM ciclista WHERE nomequipo = 'Artiach' OR nomequipo = 'Amore Vita';
+
+-- 1.4 Listar los dorsales de los ciclistas cuya edad sea menor que 25 o mayor que 30
+SELECT dorsal FROM ciclista WHERE edad < 25 OR edad > 30;
+
+-- 1.5 Listar los dorsales de los ciclistas cuya edad este entre 28 y 32 y que además solo sean de Banesto
+SELECT dorsal FROM ciclista WHERE (edad BETWEEN 28 AND 32) AND (nomequipo = 'Banesto');
