55557c6d16f1d7fbe6be39ee1a7374f83a0dbbee
Oscar Megía López <pfgytk@gmail.com>
2020-02-04 14:05:07 +0100

    Empezada Hoja de Ejercicios 7 en academia

---
+++ m2/Ejercicios/20200204/Hoja de Ejercicios 7 SQL.sql
@@ -0,0 +1,7 @@
+﻿-- Crear una vista que permita ver los productos cuyo precio es mayor que 100. 
+-- Mostrar los campos codigo articulo, seccion y precio.
+CREATE VIEW CONSULTA1b AS SELECT codigo_articulo, seccion, precio FROM productos WHERE precio > 100;
+
+CREATE VIEW CONSULTA2b AS SELECT * FROM consulta1b WHERE seccion = 'DEPORTES';
+
+INSERT INTO CONSULTA1b (codigo_articulo, seccion, precio) VALUES ('AR90', 'Novedades', 5);
