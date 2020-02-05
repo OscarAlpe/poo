215d8abc1296dc2afcc8fa2d98ddd616225511d2
Oscar Megía López <pfgytk@gmail.com>
2020-02-05 14:05:32 +0100

    Terminada Hoja de Ejercicios 7 y tablas oficinas, visitas y clientes en academia

--- m2/Ejercicios/20200204/Hoja de Ejercicios 7 SQL.sql
+++ m2/Ejercicios/20200204/Hoja de Ejercicios 7 SQL.sql
@@ -1,7 +1,29 @@
-﻿-- Crear una vista que permita ver los productos cuyo precio es mayor que 100. 
+-- 1- Crear una vista que permita ver los productos cuyo precio es mayor que 100. 
 -- Mostrar los campos codigo articulo, seccion y precio.
 CREATE VIEW CONSULTA1b AS SELECT codigo_articulo, seccion, precio FROM productos WHERE precio > 100;
 
+-- 3- Crear una vista que use como tabla la consulta1b y que nos  muestre todos los productos cuya sección sea DEPORTES
 CREATE VIEW CONSULTA2b AS SELECT * FROM consulta1b WHERE seccion = 'DEPORTES';
 
+-- Hay que poner un valor por defecto en importado porque sino da error
+ALTER TABLE PRODUCTOS alter importado set default 0;
+
+-- 5- Insertar el siguiente registro en la tabla productos a través de la vista consulta1b
 INSERT INTO CONSULTA1b (codigo_articulo, seccion, precio) VALUES ('AR90', 'Novedades', 5);
+
+-- 6- Modificar la vista consulta1b y activar el check en la vista en modo local
+-- 7- Insertar en consulta1b el siguiente registro:
+INSERT INTO CONSULTA1b (codigo_articulo, seccion, precio) VALUES ('AR91', 'Novedades', 110);
+
+-- 8- Insertar el siguiente registro en la tabla productos pero a través de la vista consulta2b
+INSERT INTO CONSULTA2b (codigo_articulo, seccion, precio) VALUES ('AR92', 'Novedades', 5);
+
+-- 9-  Modificar la vista consulta2b y activar el check en la vista en modo local
+-- 10- Insertar el siguiente registro en la tabla productos pero a través de la vista consulta2
+INSERT INTO CONSULTA2b (codigo_articulo, seccion, precio) VALUES ('AR93', 'DEPORTES', 5);
+
+-- 11- Modificar la vista consulta2b y activar el check en la vista en modo CASCADA
+-- 12- Insertar el siguiente registro en la tabla productos pero a través de la vista consulta2
+INSERT INTO CONSULTA2b (codigo_articulo, seccion, precio) VALUES ('AR94', 'DEPORTES', 200);
+
+
---
+++ m2/Ejercicios/20200205/Tablas oficinas visitas y clientes.sql
@@ -0,0 +1,32 @@
+-- Tabla oficinas
+CREATE TABLE oficinas (
+  id_ofi int NOT NULL,
+  direccion_ofi varchar(255),
+  telefono_ofi varchar(9),
+  fax_ofi varchar(9),
+  PRIMARY KEY(id_ofi),
+  REFERENCES inmuebles(id_ofi) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Tabla visitas
+CREATE TABLE visitas (
+  id_inm int NOT NULL,
+  id_cli int,
+  fecha_vis date,
+  comentario_vis varchar(255),
+  PRIMARY KEY (id_inm)
+);
+
+-- Tabla Clientes
+CREATE TABLE clientes (
+  id_cli int NOT NULL,
+  nombre_cli varchar(255),
+  direccion_cli varchar(255),
+  telefono_cli varchar(9),
+  inmueble_preferido_cli int,
+  importe_maximo_cli int,
+  PRIMARY KEY(id_cli),
+  REFERENCES visitas(id_cli) ON DELETE CASCADE ON UPDATE CASCADE,
+  REFERENCES facturas(id_cli) ON DELETE CASCADE ON UPDATE CASCADE,
+  REFERENCES contrato(id_cli) ON DELETE CASCADE ON UPDATE CASCADE
+);
