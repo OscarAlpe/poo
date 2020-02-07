368fb624dd1fd47fcafc443a398d17bb56b584f5
Oscar Megía López <pfgytk@gmail.com>
2020-02-07 14:07:39 +0100

    Actualizado creación BBDD inmobiliaria por Rubén, añadido comando SQL y generación de datos con dbForge Studio for MySQL en academia

--- m2/Ejercicios/20200206/Inmobiliaria_creacion.sql
+++ m2/Ejercicios/20200206/Inmobiliaria_creacion.sql
@@ -1,32 +1,25 @@
-CREATE DATABASE inmobiliaria;
-
-/* Andrés*/
-
-
-create table periodico(
-id_per integer NOT NULL,
-nombre_per varchar(20) DEFAULT NULL,
-direccion_per varchar(20) DEFAULT NULL,
-telefono_per integer DEFAULT NULL,
-fax_per integer DEFAULT NULL,
-nombre_pers_cont_per varchar(20) DEFAULT NULL,
-primary key (id_per)
-);
-
-create table propietario(
-id_pro integer NOT NULL,
-nombre_pro varchar(20) DEFAULT NULL,
-direccion_pro varchar(20) DEFAULT NULL,
-telefono_pro integer DEFAULT NULL,
-tipo_empresa_pro varchar (20) DEFAULT NULL,
-persona_contrato_pro varchar(20) DEFAULT NULL,
-primary key (id_pro)
-);
-
-
-/*Rubén*/
-
--- Directores
+-- CREATE DATABASE inmobiliaria;
+-- USE inmobiliaria;
+-- Modificado script por Rubén
+-- TABLAS
+DROP TABLE IF EXISTS Directores;
+DROP TABLE IF EXISTS Empleados;
+DROP TABLE IF EXISTS Entrevista;
+DROP TABLE IF EXISTS Administrativo;
+DROP TABLE IF EXISTS Pariente;
+DROP TABLE IF EXISTS Oficinas;
+DROP TABLE IF EXISTS Visitas;
+DROP TABLE IF EXISTS Clientes;
+DROP TABLE IF EXISTS Inspecciones;
+DROP TABLE IF EXISTS Inmuebles;
+DROP TABLE IF EXISTS Inmueble_Factura;
+DROP TABLE IF EXISTS Facturas;
+DROP TABLE IF EXISTS Periodico;
+DROP TABLE IF EXISTS Publicidad;
+DROP TABLE IF EXISTS Propietario;
+DROP TABLE IF EXISTS Pago;
+DROP TABLE IF EXISTS Contrato;
+/* Rubén -- Directores */
 CREATE TABLE Directores (
   id_dir            integer,
   pago_anual_dir    integer,
@@ -34,7 +27,8 @@ CREATE TABLE Directores (
 );
 ALTER TABLE Directores
   ADD PRIMARY KEY (id_dir);
--- Empleados
+
+/* Rubén -- Empleados */
 CREATE TABLE Empleados (
   id_emp            integer,
   dni_emp           varchar(9),
@@ -49,24 +43,21 @@ CREATE TABLE Empleados (
 );
 ALTER TABLE Empleados
   ADD PRIMARY KEY (id_emp);
--- Empleados
-
-
-/*David*/
-  create table Entrevista (
+/* David -- Entrevista */
+create table Entrevista (
   id_emp integer,
   id_cli integer,
-  fecha_ent date, 
+  fecha_ent date,
   comentario_ent varchar (255),
   primary key (id_emp,id_cli)
 );
-
+/* David -- Administrativo */
 CREATE table Administrativo (
   id_adm integer,
   velocidad_escri_adm integer,
   primary key (id_adm)
-  );
-
+);
+/* David -- Pariente */
 create table Pariente (
   id_par integer,
   nombre_par varchar(255),
@@ -74,75 +65,9 @@ create table Pariente (
   direccion_par varchar (255),
   telefono_par varchar(9),
   id_emp integer,
-  primary key (id_par));
-
-
-
-
-
-/*Pablo*/
-
-  CREATE TABLE Facturas(
-	id_fac int,
-	importe_fac int,
-	metodo_pago varchar(255),
-	importe_deposito int,
-	plazo_pago int,
-	observaciones varchar(255),
-	id_cli int,
-	PRIMARY KEY(id_fac)
+  primary key (id_par)
 );
-
-CREATE TABLE Inmueble_Factura(
-	id_inm int,
-	id_fac int,
-	PRIMARY KEY(id_inm,id_fac)
-);
-
-
-CREATE TABLE Pago(
-	id_pag int,
-	importe int,
-	fecha_pag date,
-	id_fac int,
-	PRIMARY KEY(id_pag)
-);
-
-
-
-/*Agustín*/
--- SQL para tablas publicidad y contrato:
-
-
-CREATE TABLE Publicidad (
-  id_pub int,
-  fecha_pub date,
-  costo_pub int,
-  id_inm int,
-  id_per int
- 
-  );
-
-ALTER TABLE Publicidad add primary key (id_pub);
-
-CREATE TABLE Contrato (
-  id_con int PRIMARY KEY,
-  importe_mensual_con int,
-  metodo_pago_con varchar(255),
-  importe_deposito_con int,
-  esta_cancelado_con int CHECK (esta_cancelado_con < 2),
-  fecha_inicio_con date,
-  fecha_fin_con date,
-  duracion_con int,
-  id_inm int REFERENCES Inmuebles(id_inm),
-  id_emp int REFERENCES Empleados(id_emp),
-  id_cli int REFERENCES Clientes(id_cli)
-  );
-
-
-/*Oscar*/
-
--- Tabla oficinas
+/* Oscar -- Oficinas */
 CREATE TABLE oficinas (
   id_ofi int NOT NULL,
   direccion_ofi varchar(255),
@@ -150,8 +75,7 @@ CREATE TABLE oficinas (
   fax_ofi varchar(9),
   PRIMARY KEY(id_ofi)
 );
-
--- Tabla visitas
+/* Oscar -- Visitas */
 CREATE TABLE visitas (
   id_inm int NOT NULL,
   id_cli int,
@@ -159,32 +83,26 @@ CREATE TABLE visitas (
   comentario_vis varchar(255),
   PRIMARY KEY (id_inm, id_cli, fecha_vis)
 );
-
--- Tabla Clientes
+/* Oscar -- Clientes */
 CREATE TABLE clientes (
   id_cli int NOT NULL,
   nombre_cli varchar(255),
   direccion_cli varchar(255),
   telefono_cli varchar(9),
-  inmueble_preferido_cli int,
+  inmueble_preferido_cli varchar(255),
   importe_maximo_cli int,
   PRIMARY KEY(id_cli)
 );
 
-
-
-/*Antonio*/
-
+/* Antonio -- Inspecciones */
 CREATE TABLE inspecciones(
   id_ins INTEGER PRIMARY KEY,
   fecha_ins DATE NOT NULL,
   comentario_ins VARCHAR (355) NOT NULL,
   id_emp INTEGER DEFAULT NULL,
-  id_inm INTEGER NOT NULL,
-  CONSTRAINT fkey_inmuebles_id_inm FOREIGN KEY (id_inm)
-      REFERENCES inmuebles (id_inm)
+  id_inm INTEGER NOT NULL
 );
-
+/* Antonio -- Inmuebles */
 CREATE TABLE inmuebles(
   id_inm INTEGER PRIMARY KEY,
   direccion_inm VARCHAR (255) NOT NULL,
@@ -195,69 +113,180 @@ CREATE TABLE inmuebles(
   galeria VARCHAR (50) NOT NULL,
   id_ofi INTEGER NOT NULL,
   id_pro INTEGER DEFAULT NULL,
-  id_emp INTEGER NOT NULL,
-  CONSTRAINT fkey_oficinas_id_ofi FOREIGN KEY (id_ofi)
-      REFERENCES oficinas (id_ofi),
-  CONSTRAINT fkey_empleados_id_emp FOREIGN KEY (id_emp)
-      REFERENCES empleados (id_emp)
+  id_emp INTEGER NOT NULL
+);
+/* Pablo -- Inmueble_Factura */
+CREATE TABLE Inmueble_Factura(
+ id_inm int,
+ id_fac int,
+ PRIMARY KEY(id_inm,id_fac)
+);
+/* Pablo -- Facturas */
+CREATE TABLE Facturas(
+ id_fac int,
+ importe_fac int,
+ metodo_pago varchar(255),
+ importe_deposito int,
+ plazo_pago int,
+ observaciones varchar(255),
+ id_cli int,
+ PRIMARY KEY(id_fac)
+);
+/* Andrés -- Periodico */
+create table periodico(
+  id_per integer NOT NULL,
+  nombre_per varchar(20) DEFAULT NULL,
+  direccion_per varchar(20) DEFAULT NULL,
+  telefono_per integer DEFAULT NULL,
+  fax_per integer DEFAULT NULL,
+  nombre_pers_cont_per varchar(20) DEFAULT NULL,
+  primary key (id_per)
+);
+/* Agustín -- Publicidad */
+ CREATE TABLE Publicidad (
+  id_pub int,
+  fecha_pub date,
+  costo_pub int,
+  id_inm int,
+  id_per int
+);
+ALTER TABLE Publicidad add primary key (id_pub);
+/* Andrés -- Propietario */
+create table propietario(
+  id_pro integer NOT NULL,
+  nombre_pro varchar(20) DEFAULT NULL,
+  direccion_pro varchar(20) DEFAULT NULL,
+  telefono_pro integer DEFAULT NULL,
+  tipo_empresa_pro varchar (20) DEFAULT NULL,
+  persona_contrato_pro varchar(20) DEFAULT NULL,
+  primary key (id_pro)
+);
+/* Pablo -- Pago */
+CREATE TABLE Pago (
+ id_pag int,
+ importe int,
+ fecha_pag date,
+ id_fac int,
+ PRIMARY KEY (id_pag)
+);
+/* Agustín -- Contrato */
+CREATE TABLE Contrato (
+  id_con integer PRIMARY KEY,
+  importe_mensual_con integer,
+  metodo_pago_con varchar(255),
+  importe_deposito_con int,
+  esta_cancelado_con integer CHECK (esta_cancelado_con < 2),
+  fecha_inicio_con date,
+  fecha_fin_con date,
+  duracion_con integer,
+  id_inm integer,
+  id_emp integer,
+  id_cli integer
 );
 
-alter table periodico add CONSTRAINT FK_publicidad_id_pub FOREIGN KEY (id_per)
-REFERENCES publicidad (id_pub) ON DELETE CASCADE ON UPDATE CASCADE;
-
-alter table propietario add CONSTRAINT FK_inmuebles_id_inm FOREIGN KEY (id_pro)
-REFERENCES inmuebles (id_inm) ON DELETE CASCADE ON UPDATE CASCADE;
-
+-- Relaciones de empleados
+/* Rubén -- Directores 1->1(son) Empleados */
+ALTER TABLE Directores
+  ADD CONSTRAINT fk_empleados_son_directores
+    FOREIGN KEY (id_dir)
+      REFERENCES Empleados (id_emp);
+/* Rubén/David -- Administrativos 1->1(son) Empleados */
+ALTER TABLE Administrativo
+  ADD CONSTRAINT fk_empleados_son_administrativos
+    FOREIGN KEY (id_adm)
+      REFERENCES Empleados (id_emp);
+/* Rubén -- Empleados n->1(pertenecen) Oficina */
 ALTER TABLE Empleados
-  ADD CONSTRAINT fk_oficina
+  ADD CONSTRAINT fk_empleados_de_oficina
     FOREIGN KEY (id_ofi)
       REFERENCES Oficinas (id_ofi);
-
+/* David -- Empleados n<->n(entrevistan) Clientes */
 alter table Entrevista
-  add constraint fk_Empleados
+  add constraint fk_Empleados_entrevistan
     foreign key (id_emp)
      references Empleados(id_emp);
-  
- 
 alter table Entrevista
-  add constraint fk_Clientes
+  add constraint fk_Clientes_entrevistan
     foreign key (id_cli)
      references Clientes (id_cli);
-
-alter table Administrativo
-  add constraint fk_Empleados
-    foreign key (id_adm)
-     references Empleados (id_emp);
-
-
+/* David -- Pariente n->1(pertenecen) Empleados */
 alter table Pariente
-add constraint fk_Empleados
-foreign key (id_emp)
-references Empleados(id_emp);  
-
+  add constraint fk_Pariente_de_empleado
+    foreign key (id_emp)
+      references Empleados(id_emp); 
+/* Rubén/Oscar Clientes n<->n(visitan) Clientes */
+ALTER TABLE Visitas
+  ADD CONSTRAINT fk_Clientes_visitan
+    FOREIGN KEY (id_cli)
+      REFERENCES Clientes (id_cli);
+ALTER TABLE Visitas
+  ADD CONSTRAINT fk_Inmuebles_visitan
+    FOREIGN KEY (id_inm)
+      REFERENCES Inmuebles (id_inm);
+/* Rubén/? Empleados n<->n(inspeccionan) Inmuebles */
+ALTER TABLE Inspecciones
+  ADD CONSTRAINT fk_Emplados_inspeccionan
+    FOREIGN KEY (id_emp)
+      REFERENCES Empleados(id_emp);
+ALTER TABLE Inspecciones
+  ADD CONSTRAINT fk_Inmuebles_inspeccionan
+    FOREIGN KEY (id_inm)
+      REFERENCES Inmuebles(id_inm);
+/* Rubén/?  Oficina 1->n(de) Inmueble */
+ALTER TABLE Inmuebles
+  ADD CONSTRAINT fk_Oficina_de_Inmueble
+    FOREIGN KEY (id_ofi)
+      REFERENCES Oficinas(id_ofi);
+/* Rubén/?  Propietario 1->n(de) Inmueble */
+ALTER TABLE Inmuebles
+  ADD CONSTRAINT fk_Propietario_de_Inmueble
+    FOREIGN KEY (id_pro)
+      REFERENCES Propietario(id_pro);
+/* Rubén/?  Empleado 1->n(de) Inmueble */
+ALTER TABLE Inmuebles
+  ADD CONSTRAINT fk_Encargado_de_Inmueble
+    FOREIGN KEY (id_emp)
+      REFERENCES Empleados(id_emp);
+/* ? Inmuebles n<->n(en) Facturas */
 ALTER TABLE Inmueble_Factura
-ADD CONSTRAINT FKInmueble_FacturaInmuebles
-FOREIGN KEY (id_inm) REFERENCES Inmuebles(id_inm);
-
+  ADD CONSTRAINT FKInmueble_FacturaInmuebles
+    FOREIGN KEY (id_inm)
+      REFERENCES Inmuebles(id_inm);
 ALTER TABLE Inmueble_Factura
-ADD CONSTRAINT FKInmueble_FacturaFacturas
-FOREIGN KEY (id_fac) REFERENCES Facturas(id_fac);
-
+  ADD CONSTRAINT FKInmueble_FacturaFacturas
+    FOREIGN KEY (id_fac)
+      REFERENCES Facturas(id_fac);
+/* ? Clientes 1->n(pagan) Facturas */
 ALTER TABLE Facturas
-ADD CONSTRAINT FKFacturasClientes
-FOREIGN KEY (id_cli) REFERENCES Clientes(id_cli);
+  ADD CONSTRAINT FKFacturasClientes
+    FOREIGN KEY (id_cli)
+      REFERENCES Clientes(id_cli);
+/* Rubén/Agustín Periodicos n<->n(publicitan) Inmuebles */
+ALTER TABLE Publicidad
+  ADD CONSTRAINT fk_Periodico_publicita
+    FOREIGN KEY (id_per)
+      REFERENCES Periodico(id_per);
+ALTER TABLE Publicidad
+  ADD CONSTRAINT fk_Inmuebles_publicitados
+    FOREIGN KEY (id_inm)
+      REFERENCES Inmuebles(id_inm);
 
-ALTER TABLE Facturas
-ADD CONSTRAINT FKFacturaPago
-FOREIGN KEY (id_fac) REFERENCES Pago(id_fac);
+/* Agustín Clientes n<->n(contratan)(Empleado/inmueble) */
+ALTER TABLE Contrato
+  ADD CONSTRAINT fk_Clientes_contratan
+    FOREIGN KEY(id_cli)
+      REFERENCES Clientes(id_cli);
+ALTER TABLE Contrato
+  ADD CONSTRAINT fk_Empleado_contratado
+    FOREIGN KEY(id_emp)
+      REFERENCES empleados(id_emp); -- ???
+ALTER TABLE Contrato
+  ADD CONSTRAINT fk_Inmuebles_contratado
+    FOREIGN KEY(id_inm)
+      REFERENCES Inmuebles(id_inm);
 
-ALTER TABLE Publicidad add foreign key (id_inm) REFERENCES Inmuebles(id_inm);
-ALTER TABLE Publicidad add foreign key (id_per) REFERENCES Periodico(id_per);
-
-ALTER TABLE inmuebles ADD FOREIGN KEY (id_ofi) REFERENCES oficinas(id_ofi) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE visitas ADD FOREIGN KEY (id_cli) REFERENCES clientes(id_cli) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE facturas ADD FOREIGN KEY (id_cli) REFERENCES clientes(id_cli) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE contrato ADD FOREIGN KEY (id_cli) REFERENCES clientes(id_cli) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE visitas ADD CONSTRAINT fkey_visitas_id_inm FOREIGN KEY (id_inm) REFERENCES inmuebles(id_inm);
+/* Rubén/Pablo */
+ALTER TABLE Pago
+  ADD CONSTRAINT FKFacturaPago
+    FOREIGN KEY (id_fac)
+      REFERENCES Facturas(id_fac);
