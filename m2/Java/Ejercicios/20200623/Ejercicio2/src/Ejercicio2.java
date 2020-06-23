ef6a7d78d2250249983b0f8f2d2e9b14500af653
Oscar Megía López <pfgytk@gmail.com>
2020-06-23 14:16:01 +0200

    Ejercicios en Java

---
+++ m2/Java/Ejercicios/20200623/Ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio2 {
+
+  public static void main(String[] args) {
+    Double a = new Double(0.0);
+    Double b = new Double(0.0);
+
+    String sa = JOptionPane.showInputDialog("Introduce número a:");
+    a = a.parseDouble(sa);
+    
+    String sb = JOptionPane.showInputDialog("Introduce número b:");
+    b = b.parseDouble(sb);
+
+    if (a > b) {
+      System.out.println("a es mayor que b");
+    } else if (b > a) {
+      System.out.println("b es mayor que s");
+    } else {
+      System.out.println("a y b son iguales");
+    }
+  }
+}
